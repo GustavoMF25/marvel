2ce9595b21c4f26ec29bc992d1180a1cbd789151
Gustavo Fernandes <gustavomarcelinofernandes25@gmail.com>
2021-10-19 19:34:56 -0300

    Adicionando o bootstrap e criando a tela welcome

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet">
+
+
+        <!--Bootstrap-->
+        <link href="{{asset('assets/bootstrap-5/css/bootstrap.min.css')}}" rel="stylesheet" />
+        <style>
+            body {
+                font-family: "RobotoCondensed Bold", sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            @if (Route::has('login'))
+            <div>
+                @auth
+                <a href="{{ url('/dashboard') }}">Dashboard</a>
+                @else
+                <a href="{{ route('login') }}">Log in</a>
+
+                @if (Route::has('register'))
+                <a href="{{ route('register') }}">Register</a>
+                @endif
+                @endauth
+            </div>
+            @endif          
+        </div>
+        <script src="{{ asset('assets/bootstrap-5/js/bootstrap.bundle.min.js') }}"></script>
+    </body>
+</html>
